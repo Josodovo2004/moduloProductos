2b0f2d046b6c823eb03b39b24652bdab7aed3c5b
JoseAguilar <joseaguilarsalazar2004@gmail.com>
2024-10-17 17:40:53 -0500

    feat: added django comands in order to automate databse filling using csv files in the csv/ folder

--- core/management/commands/import_segmento.py
+++ core/management/commands/import_segmento.py
@@ -1,6 +1,7 @@
 import csv
 from django.core.management.base import BaseCommand
 from core.models import SegmentoProducto, FamiliaProducto, ClaseProducto, Producto
+from django.db.utils import IntegrityError
 
 class Command(BaseCommand):
     help = 'Load data from CSV files into the database'
@@ -10,29 +11,41 @@ class Command(BaseCommand):
         with open('core/management/commands/csv/segmentos.csv', newline='', encoding='windows-1252') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                SegmentoProducto.objects.get_or_create(codigo=row['CODIGO DE SEGMENTO'], descripcion=row['DESCRIPCION DE SEGMENTO'])
+                try:
+                    SegmentoProducto.objects.get_or_create(codigo=row['CODIGO DE SEGMENTO'], descripcion=row['DESCRIPCION DE SEGMENTO'])
+                except IntegrityError:
+                    pass
         self.stdout.write(self.style.SUCCESS('Successfully loaded SegmentoProducto data.'))
 
         # Load FamiliaProducto
         with open('core/management/commands/csv/familias.csv', newline='', encoding='windows-1252') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                segmento = SegmentoProducto.objects.get(codigo=row['CODIGO DE SEGMENTO'])
-                FamiliaProducto.objects.get_or_create(codigo=row['CODIGO DE FAMILIA'], segmento=segmento, descripcion=row['DESCRIPCION DE FAMILIA'])
+                try:
+                    segmento = SegmentoProducto.objects.get(codigo=row['CODIGO DE SEGMENTO'])
+                    FamiliaProducto.objects.get_or_create(codigo=row['CODIGO DE FAMILIA'], segmento=segmento, descripcion=row['DESCRIPCION DE FAMILIA'])
+                except IntegrityError:
+                    pass
         self.stdout.write(self.style.SUCCESS('Successfully loaded FamiliaProducto data.'))
 
         # Load ClaseProducto
         with open('core/management/commands/csv/clases.csv', newline='', encoding='windows-1252') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                familia = FamiliaProducto.objects.get(codigo=row['CODIGO DE FAMILIA'])
-                ClaseProducto.objects.get_or_create(codigo=row['CODIGO DE CLASE'], familia=familia, descripcion=row['DESCRIPCION DE CLASE'])
+                try:
+                    familia = FamiliaProducto.objects.get(codigo=row['CODIGO DE FAMILIA'])
+                    ClaseProducto.objects.get_or_create(codigo=row['CODIGO DE CLASE'], familia=familia, descripcion=row['DESCRIPCION DE CLASE'])
+                except IntegrityError:
+                    pass
         self.stdout.write(self.style.SUCCESS('Successfully loaded ClaseProducto data.'))
 
         # Load Producto
         with open('core/management/commands/csv/productos.csv', newline='', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                clase = ClaseProducto.objects.get(codigo=row['CODIGO DE CLASE'])
-                Producto.objects.get_or_create(codigo=str(row['CODIGO DE PRODUCTO']).split('-')[0], clase=clase, descripcion=row['DESCRPCION DE PRODUCTO'])
+                try:
+                    clase = ClaseProducto.objects.get(codigo=row['CODIGO DE CLASE'])
+                    Producto.objects.get_or_create(codigo=str(row['CODIGO DE PRODUCTO']).split('-')[0], clase=clase, descripcion=row['DESCRPCION DE PRODUCTO'])
+                except IntegrityError:
+                    pass
         self.stdout.write(self.style.SUCCESS('Successfully loaded Producto data.'))
