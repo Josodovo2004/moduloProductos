e6dca789cd7cc21724a8ef609d8d6a956ef4f5af
Jose <joseaguilarsalazar2004@gmail.com>
2024-10-22 08:43:05 -0500

    refactor: if enviroment in development, auth is not necessary

--- core/views.py
+++ core/views.py
@@ -21,63 +21,80 @@ class SegmentoProductoListCreateView(generics.ListCreateAPIView):
     serializer_class = SegmentoProductoSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = SegmentoProductoFilter
-
+    authentication_classes = [CustomJWTAuthentication]
+    permission_classes = []
 
 class SegmentoProductoRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = SegmentoProducto.objects.all()
     serializer_class = SegmentoProductoSerializer
-
+    authentication_classes = [CustomJWTAuthentication]
+    permission_classes = []
 
 class FamiliaProductoListCreateView(generics.ListCreateAPIView):
     queryset = FamiliaProducto.objects.all()
     serializer_class = FamiliaProductoSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = FamiliaProductoFilter
-
+    authentication_classes = [CustomJWTAuthentication]
+    permission_classes = []
 
 class FamiliaProductoRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = FamiliaProducto.objects.all()
     serializer_class = FamiliaProductoSerializer
-
+    authentication_classes = [CustomJWTAuthentication]
+    permission_classes = []
 
 class ClaseProductoListCreateView(generics.ListCreateAPIView):
     queryset = ClaseProducto.objects.all()
     serializer_class = ClaseProductoSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = ClaseProductoFilter
-
+    authentication_classes = [CustomJWTAuthentication]
+    permission_classes = []
 
 class ClaseProductoRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = ClaseProducto.objects.all()
     serializer_class = ClaseProductoSerializer
-
+    authentication_classes = [CustomJWTAuthentication]
+    permission_classes = []
 
 class ProductoListCreateView(generics.ListCreateAPIView):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = ProductoFilter
-
+    authentication_classes = [CustomJWTAuthentication]
+    permission_classes = []
 
 class ProductoRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
+    authentication_classes = [CustomJWTAuthentication]
+    permission_classes = []
 
 class TipoPrecioListCreateView(generics.ListCreateAPIView):
     queryset = TipoPrecio.objects.all()
     serializer_class = TipoPrecioSerializer
+    authentication_classes = [CustomJWTAuthentication]
+    permission_classes = []
 
 class TipoPrecioRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = TipoPrecio.objects.all()
     serializer_class = TipoPrecioSerializer
+    authentication_classes = [CustomJWTAuthentication]
+    permission_classes = []
 
 class UnidadMedidaListCreateView(generics.ListCreateAPIView):
     queryset = UnidadMedida.objects.all()
     serializer_class = UnidadMedidaSerializer
+    authentication_classes = [CustomJWTAuthentication]
+    permission_classes = []
 
 class UnidadMedidaRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = UnidadMedida.objects.all()
     serializer_class = UnidadMedidaSerializer
+    authentication_classes = [CustomJWTAuthentication]
+    permission_classes = []
 
 # Secure CRUD views for Item
 class ItemListCreateView(generics.ListCreateAPIView):
