2c270fc2af090d59d443a5ea29d6c06854eca380
JoseAguilar <joseaguilarsalazar2004@gmail.com>
2024-11-06 15:55:34 -0500

    feat: now item serializer gives all the important data of the item in one single view

--- core/views.py
+++ core/views.py
@@ -135,7 +135,7 @@ class ItemListCreateView(generics.ListCreateAPIView):
     def list(self, request, *args, **kwargs):
         response = super().list(request, *args, **kwargs)
         data = response.data
-        if not request.data['resupuesta_simple']:
+        if 'respuesta_simple' not in request.data:
             for i in range(len(data['results'])):
                 if isinstance(data['results'][i], dict) and 'id' in data['results'][i]:
                     item = self.get_queryset().filter(id=data['results'][i]['id']).first()
